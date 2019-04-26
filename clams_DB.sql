22662d9f19481280fac2a1af1120085db619a633
李世超 <1030968488@qq.com>
2019-04-26 18:08:15 +0800

    添加数据库sql

---
+++ clams_DB.sql
@@ -0,0 +1,116 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.5.27 : Database - clams
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`clams` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `clams`;
+
+/*Table structure for table `sys_permission` */
+
+DROP TABLE IF EXISTS `sys_permission`;
+
+CREATE TABLE `sys_permission` (
+  `id` bigint(20) NOT NULL COMMENT '主键',
+  `name` varchar(128) NOT NULL COMMENT '资源名称',
+  `type` varchar(32) NOT NULL COMMENT '资源类型：menu,button,',
+  `url` varchar(128) DEFAULT NULL COMMENT '访问url地址',
+  `percode` varchar(128) DEFAULT NULL COMMENT '权限代码字符串',
+  `parentid` bigint(20) DEFAULT NULL COMMENT '父结点id',
+  `parentids` varchar(128) DEFAULT NULL COMMENT '父结点id列表串',
+  `sortstring` varchar(128) DEFAULT NULL COMMENT '排序号',
+  `available` char(1) DEFAULT NULL COMMENT '是否可用,1：可用，0不可用',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `sys_role` */
+
+DROP TABLE IF EXISTS `sys_role`;
+
+CREATE TABLE `sys_role` (
+  `id` varchar(36) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `available` char(1) DEFAULT NULL COMMENT '是否可用,1：可用，0不可用',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `sys_role_permission` */
+
+DROP TABLE IF EXISTS `sys_role_permission`;
+
+CREATE TABLE `sys_role_permission` (
+  `id` varchar(36) NOT NULL,
+  `sys_role_id` varchar(32) NOT NULL COMMENT '角色id',
+  `sys_permission_id` varchar(32) NOT NULL COMMENT '权限id',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `sys_user` */
+
+DROP TABLE IF EXISTS `sys_user`;
+
+CREATE TABLE `sys_user` (
+  `id` varchar(36) NOT NULL COMMENT '主键',
+  `usercode` varchar(32) NOT NULL COMMENT '账号',
+  `username` varchar(64) NOT NULL COMMENT '姓名',
+  `password` varchar(32) NOT NULL COMMENT '密码',
+  `salt` varchar(64) DEFAULT NULL COMMENT '盐',
+  `locked` char(1) DEFAULT NULL COMMENT '账号是否锁定，1：锁定，0未锁定',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `sys_user_role` */
+
+DROP TABLE IF EXISTS `sys_user_role`;
+
+CREATE TABLE `sys_user_role` (
+  `id` varchar(36) NOT NULL,
+  `sys_user_id` varchar(32) NOT NULL,
+  `sys_role_id` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+  `username` char(20) NOT NULL COMMENT '用户名',
+  `password` char(20) NOT NULL COMMENT '密码',
+  `user_type` char(20) NOT NULL COMMENT '用户类型',
+  `create_time` date DEFAULT NULL COMMENT '创建时间',
+  `updata_time` date DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
+
+/*Table structure for table `video_comment` */
+
+DROP TABLE IF EXISTS `video_comment`;
+
+CREATE TABLE `video_comment` (
+  `comment_id` int(11) NOT NULL AUTO_INCREMENT,
+  `video_url` char(100) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  `comment_content` char(100) NOT NULL,
+  `user_location` char(100) DEFAULT NULL,
+  `user_address` char(100) DEFAULT NULL,
+  `comment_time` date DEFAULT NULL,
+  `state` int(2) DEFAULT '0',
+  PRIMARY KEY (`comment_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
