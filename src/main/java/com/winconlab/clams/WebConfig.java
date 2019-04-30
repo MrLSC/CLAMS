c9e424d9ecbccced9a46069a12ecfad7cb7743c4
李世超 <1030968488@qq.com>
2019-04-30 11:13:39 +0800

    开启事物

--- src/main/java/com/winconlab/clams/WebConfig.java
+++ src/main/java/com/winconlab/clams/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -13,6 +14,7 @@ import java.nio.charset.Charset;
 import java.util.List;
 
 @Configuration
+@EnableTransactionManagement
 //@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
