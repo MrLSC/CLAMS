8149a1d9546e09a653c4fe3a4a6cb89d84c81ecf
李世超 <1030968488@qq.com>
2019-04-30 11:12:41 +0800

    增加logback配置

--- src/main/java/com/winconlab/clams/WebConfig.java
+++ src/main/java/com/winconlab/clams/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -12,6 +13,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import java.nio.charset.Charset;
 import java.util.List;
 
+@EnableTransactionManagement
 @Configuration
 //@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
